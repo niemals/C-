7ab85f9800a2948fb4e7a15e6d5ae5fb96b8767a
David Lee <38522671+niemals@users.noreply.github.com>
2020-09-21 22:09:14 +0800

    Add files via upload

---
+++ 1_helloworld.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+using namespace std;
+
+
+int main()    // the 'main' is the entry of program , and every program must has a 'main' function
+{
+	int a = 1;int b = 2;  //define variant   *single line 
+	int c;    
+	c=a + b;
+	cout << c<< endl;   /*print the result    *mutilte lines
+						just a space line */
+	system("pause");
+	return 0;
+	
+}
